3cdcd1415f5d022fc3fb3fe90c9fccf3cbd39e1c
Hallan <hallanjrg@gmail.com>
2016-01-17 20:18:56 -0200

    Ainda não está finalizado..mas segue o que desenvolvi hoje..

--- src/main/java/br/com/bought/common/CarrinhoVO.java
+++ src/main/java/br/com/bought/common/CarrinhoVO.java
@@ -1,12 +1,19 @@
 package br.com.bought.common;
 
 import java.util.Calendar;
+import java.util.List;
+
+import br.com.bought.enums.StatusCarrinhoENUM;
 
 public class CarrinhoVO {
 	// número sessão
 	private String numeroCarrinho;
 	private Calendar dataCarrinho;
 	private MercadoVO mercado;
+	private StatusCarrinhoENUM statusCarrinho;
+	private List<ItemCompraVO> itens;
+	
+	
 	public String getNumeroCarrinho() {
 		return numeroCarrinho;
 	}
@@ -24,5 +31,17 @@ public class CarrinhoVO {
 	}
 	public void setMercado(MercadoVO mercado) {
 		this.mercado = mercado;
+	}
+	public StatusCarrinhoENUM getStatusCarrinho() {
+		return statusCarrinho;
+	}
+	public void setStatusCarrinho(StatusCarrinhoENUM statusCarrinho) {
+		this.statusCarrinho = statusCarrinho;
+	}
+	public List<ItemCompraVO> getItens() {
+		return itens;
+	}
+	public void setItens(List<ItemCompraVO> itens) {
+		this.itens = itens;
 	}
 }
--- src/main/java/br/com/bought/resources/CarrinhoResource.java
+++ src/main/java/br/com/bought/resources/CarrinhoResource.java
@@ -11,13 +11,18 @@ import javax.ws.rs.core.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.bought.common.CarrinhoVO;
+import br.com.bought.common.CompraVO;
 import br.com.bought.common.MercadoVO;
 import br.com.bought.common.UsuarioVO;
 import br.com.bought.dao.MercadoDAO;
 import br.com.bought.dao.UsuarioDAO;
+import br.com.bought.enums.StatusCarrinhoENUM;
+import br.com.bought.enums.StatusCompraENUM;
+import br.com.bought.utils.BoughtUtils;
 
 @RestController
 @RequestMapping("/carrinho/")
@@ -37,11 +42,37 @@ public class CarrinhoResource {
 				MercadoVO mercado = MercadoDAO.obterMercadoPorQRCode(codigoMercado);
 				if (mercado != null){
 					retorno.setMercado(mercado);
+					retorno.setStatusCarrinho(StatusCarrinhoENUM.EM_COMPRAS);
 					retorno.setDataCarrinho(Calendar.getInstance());
 					retorno.setNumeroCarrinho(UUID.randomUUID().toString());
 				}
 			}
 		}
 		return retorno;
+	}
+	
+	/**
+	 * Este método é acionado para finalizar o carrinho de compras.
+	 * Tem como tarefa gerar uma compra na base de dados e retornar o objeto CompraVO 
+	 * para o apk.
+	 * @param carrinho
+	 * @param usuario
+	 * @return
+	 */
+	//TODO ATUALIZAR INFORMAÇÕES DO CARRINHO NA BASE.
+	//TODO GERAR COMPRA NA BASE.
+	@RequestMapping(value =  "/inserir", method = RequestMethod.POST)
+	public CompraVO finalizarCarrinho(@RequestBody CarrinhoVO carrinho, 
+			@RequestBody UsuarioVO usuario){
+		CompraVO compra = null;
+		if(carrinho != null && usuario != null){
+			compra = new CompraVO();
+			carrinho.setStatusCarrinho(StatusCarrinhoENUM.FINALIZADO);
+			compra.setCarrinho(carrinho);
+			compra.setDataGeracaoCompra(Calendar.getInstance());
+			compra.setStatusCompra(StatusCompraENUM.AGUARDANDO_PAGAMENTO);
+			compra.setValor(BoughtUtils.getValorCompraFromCarrinho(carrinho));
+		}
+		return compra;
 	}
 }
